bb32c12d6e07cd984a095ed1867e2005e97d388f
ame <shachi10969@gmail.com>
2023-06-27 13:21:19 +0900

    Initial Commit

---
+++ BigMoji/BigMojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigMojiApp.swift
+//  BigMoji
+//
+//  Created by t&a on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct BigMojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
