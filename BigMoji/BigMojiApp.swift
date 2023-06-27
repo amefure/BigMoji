40a450dfeecb71a285003154ff1b31d8daba8b1d
ame <shachi10969@gmail.com>
2023-06-28 08:26:00 +0900

    Add Firebase Analytics

--- BigMoji/BigMojiApp.swift
+++ BigMoji/BigMojiApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import FirebaseCore
 import GoogleMobileAds
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
     }
