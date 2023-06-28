d8770a32e7ad855dc6f84512cd91989587409d6f
ame <shachi10969@gmail.com>
2023-06-28 09:47:06 +0900

    Add DeviceSizeViewModel

---
+++ BigMoji/ViewModels/DeviceSizeViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  DeviceSizeViewModel.swift
+//  BigMoji
+//
+//  Created by t&a on 2023/06/28.
+//
+
+import UIKit
+
+class DeviceSizeViewModel {
+    
+    public let deviceWidth = UIScreen.main.bounds.width
+    public let deviceHeight = UIScreen.main.bounds.height
+    // 端末設定のカレンダー
+    public let calendar = Calendar.current
+    
+    public var isSESize:Bool {
+        if deviceWidth < 400{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    public var isiPadSize:Bool {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    public var isLandscape:Bool {
+        if UIDevice.current.orientation == .landscapeLeft || UIDevice.current.orientation == .landscapeRight {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+}
