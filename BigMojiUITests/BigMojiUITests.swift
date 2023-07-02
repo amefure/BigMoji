0c2078d8380dcd945e9b3c3481a61e5893ad0dcf
ame <shachi10969@gmail.com>
2023-07-03 07:36:13 +0900

    Added UITests

--- BigMojiUITests/BigMojiUITests.swift
+++ BigMojiUITests/BigMojiUITests.swift
@@ -22,12 +22,73 @@ final class BigMojiUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testPlaneTextShow() throws {
+        /// シンプルに文字のみを表示するTest
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let collectionViewsQuery = app.collectionViews
+        
+        let textView = collectionViewsQuery.children(matching: .cell).element(boundBy: 1).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element
+        textView.tap()
+        textView.typeText("Hello")
+        app.toolbars["Toolbar"].buttons["閉じる"].tap()
+        collectionViewsQuery.containing(.other, identifier:"垂直スクロールバー, 2ページ").element.swipeUp()
+        collectionViewsQuery.buttons["SHOW"].tap()
+        XCUIApplication().navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["戻る"].tap()
+        XCTAssertTrue(collectionViewsQuery.buttons["SHOW"].exists)
+    }
+    
+    func testCustomTextShow() throws {
+        /// 文字に装飾を施して表示するTest
+        let app = XCUIApplication()
+        app.launch()
+        
+        let collectionViewsQuery = app.collectionViews
+        
+        let textView = collectionViewsQuery.children(matching: .cell).element(boundBy: 1).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element
+        textView.tap()
+        textView.typeText("Hello")
+        app.toolbars["Toolbar"].buttons["閉じる"].tap()
+        
+        // 赤文字
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 3).scrollViews.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .button).element(boundBy: 0).tap()
+        // 緑背景
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 5).scrollViews.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .button).element(boundBy: 2).tap()
+        
+        collectionViewsQuery.buttons["bold"].tap()
+        collectionViewsQuery.buttons["スペース"].tap()
+        collectionViewsQuery.containing(.other, identifier:"垂直スクロールバー, 2ページ").element.swipeUp()
+        collectionViewsQuery.buttons["SHOW"].tap()
+        
+        XCUIApplication().navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["戻る"].tap()
+        XCTAssertTrue(collectionViewsQuery.buttons["SHOW"].exists)
+    }
+    
+    func testSelectColorTextShow() throws {
+        /// 文字に装飾を施して表示するTest
+        let app = XCUIApplication()
+        app.launch()
+        
+        let collectionViewsQuery = app.collectionViews
+        
+        let textView = collectionViewsQuery.children(matching: .cell).element(boundBy: 1).children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element
+        textView.tap()
+        textView.typeText("Hello")
+        app.toolbars["Toolbar"].buttons["閉じる"].tap()
+        
+        
+        let button = collectionViewsQuery.children(matching: .cell).element(boundBy: 3).scrollViews.otherElements.otherElements["色の選択"].children(matching: .other).element.children(matching: .button).element
+        button.tap()
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.otherElements["暗いシアン 35"].tap()
+        elementsQuery.buttons["閉じる"].tap()
+                
+        collectionViewsQuery.containing(.other, identifier:"垂直スクロールバー, 2ページ").element.swipeUp()
+        collectionViewsQuery.buttons["SHOW"].tap()
+        
+        XCUIApplication().navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["戻る"].tap()
+        XCTAssertTrue(collectionViewsQuery.buttons["SHOW"].exists)
     }
 
     func testLaunchPerformance() throws {
